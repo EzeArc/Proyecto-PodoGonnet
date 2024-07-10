577e95347fea1d3181d8b18212ff8c279ab084e6
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-10 18:53:12 -0300

    feat(admin-tables): Actualización de la ui de las tablas del admin -Mejora de colores y legibilidad -Espaciado -Botones

--- PodoFrontGonnet/src/components/ServiceTable.jsx
+++ PodoFrontGonnet/src/components/ServiceTable.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import ContextoAdministrador from "./../context/ContextLoginRegister";
-import "../pages/css/Tabla-Turnos-Admin.css";
+import "../pages/css/Tablas-Admin.css";
 
 export const ServiceTable = () => {
   const { eliminarServicioAdmin, listaServicios, listaServiciosAdmin } =
@@ -11,7 +11,7 @@ export const ServiceTable = () => {
   }, []);
 
   return (
-    <section className="tabla-turnos-admin my-5">
+    <section className="tabla-admin my-5">
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
           <h2 className="accordion-header">
@@ -31,123 +31,91 @@ export const ServiceTable = () => {
             className="accordion-collapse collapse show"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
+            <div className="accordion-body tabla-admin-body">
               {listaServicios && listaServicios.length > 0 ? (
                 <table className="table align-middle">
                   <thead className="tabla-header">
                     <tr>
-                      <th scope="col">Imagen</th>
-                      <th scope="col">Nombre del servicio</th>
-                      <th scope="col">Descripción del servicio</th>
-                      <th scope="col">Costo</th>
-                      <th scope="col">Estado del turno</th>
-                      <th scope="col">Modificar</th>
-                      <th scope="col">Cancelar</th>
+                      <th scope="col" className="m-auto text-center">
+                        Imagen
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Nombre del servicio
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Descripción del servicio
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Costo
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Estado del turno
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Modificar
+                      </th>
+                      <th scope="col" className="m-auto text-center">
+                        Cancelar
+                      </th>
                     </tr>
                   </thead>
-                  {listaServicios.map((servicio) =>
-                    servicio.estado === true ? (
-                      <tbody key={servicio.id}>
-                        <tr>
-                          <td>
+                  {listaServicios.map((servicio) => (
+                    <tbody key={servicio.id}>
+                      <tr>
+                        <td className="m-auto">
+                          <img
+                            className="table-img"
+                            width={60}
+                            height={60}
+                            src={`data:${servicio.imagen.mime};base64,${servicio.imagen.content}`}
+                            alt={servicio.nombre}
+                          />
+                        </td>
+                        <td className="m-auto p-4 service-name">
+                          {servicio.nombre}
+                        </td>
+                        <td className="m-auto p-4">{servicio.descripcion}</td>
+                        <td className="m-auto p-4 service-price">
+                          ${servicio.costo}
+                        </td>
+                        <td className="m-auto p-4">
+                          {servicio.estado === true ? (
+                            <span className="habilitado">Habilitado</span>
+                          ) : (
+                            <span className="deshabilitado">Deshabilitado</span>
+                          )}
+                        </td>
+                        <td className="m-auto">
+                          <button
+                            className="tabla-admin-btn admin-btn"
+                            // onClick={(e) => {
+                            //   eliminarservicio(e, listaservicios.id);
+                            // }}
+                          >
                             <img
-                              className="service-img"
-                              src={`data:${servicio.imagen.mime};base64,${servicio.imagen.content}`}
-                              alt={servicio.nombre}
+                              className="admin-icons"
+                              src="/src/assets/ImagenesOptimizadas/icons/calendar-cog.svg"
+                              alt="Modificar servicio"
                             />
-                          </td>
-                          <td>{servicio.nombre}</td>
-                          <td>{servicio.descripcion}</td>
-                          <td>{servicio.costo}</td>
-                          <td>
-                            {servicio.estado === true ? (
-                              <span>Habilitado</span>
-                            ) : (
-                              <span>Deshabilitado</span>
-                            )}
-                          </td>
-                          <td>
-                            <button
-                              className="tabla-turno-btn admin-turno-btn"
-                              // onClick={(e) => {
-                              //   eliminarservicio(e, listaservicios.id);
-                              // }}
-                            >
-                              <img
-                                className="admin-icons"
-                                src="/src/assets/ImagenesOptimizadas/icons/calendar-cog.svg"
-                                alt="Modificar servicio"
-                              />
-                            </button>
-                          </td>
-                          <td>
-                            <button
-                              className="tabla-turno-btn admin-turno-btn"
-                              onClick={(e) => {
-                                eliminarServicioAdmin(e, servicio.id);
-                              }}
-                            >
-                              <img
-                                className="admin-icons"
-                                src="/src/assets/ImagenesOptimizadas/icons/calendar-x.svg"
-                                alt="Eliminar Turno"
-                              />
-                            </button>
-                          </td>
-                        </tr>
-                      </tbody>
-                    ) : (
-                      <tbody key={servicio.id}>
-                        <tr>
-                          <td>
+                          </button>
+                        </td>
+                        <td className="m-auto">
+                          <button
+                            className="tabla-admin-btn admin-btn"
+                            onClick={(e) => {
+                              eliminarServicioAdmin(e, servicio.id);
+                            }}
+                          >
                             <img
-                              className="service-img"
-                              src={`data:${servicio.imagen.mime};base64,${servicio.imagen.content}`}
-                              alt={servicio.nombre}
+                              className="admin-icons"
+                              src="/src/assets/ImagenesOptimizadas/icons/calendar-x.svg"
+                              alt="Eliminar Turno"
                             />
-                          </td>
-                          <td>{servicio.nombre}</td>
-                          <td>{servicio.descripcion}</td>
-                          <td>{servicio.costo}</td>
-                          <td>
-                            {servicio.estado === true ? (
-                              <span>Habilitado</span>
-                            ) : (
-                              <span>Deshabilitado</span>
-                            )}
-                          </td>
-                          <td>
-                            <button
-                              className="tabla-turno-btn admin-turno-btn"
-                              // onClick={(e) => {
-                              //   eliminarservicio(e, listaservicios.id);
-                              // }}
-                            >
-                              <img
-                                className="admin-icons"
-                                src="/src/assets/ImagenesOptimizadas/icons/calendar-cog.svg"
-                                alt="Modificar servicio"
-                              />
-                            </button>
-                          </td>
-                          <td>
-                            <button
-                              className="tabla-turno-btn admin-turno-btn"
-                              onClick={(e) => {
-                                eliminarServicioAdmin(e, servicio.id);
-                              }}
-                            >
-                              <img
-                                className="admin-icons"
-                                src="/src/assets/ImagenesOptimizadas/icons/calendar-x.svg"
-                                alt="Eliminar Turno"
-                              />
-                            </button>
-                          </td>
-                        </tr>
-                      </tbody>
-                    )
-                  )}
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  ))}
                 </table>
               ) : (
                 <p>No tienes turnos reservados</p>
