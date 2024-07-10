f179c03a6750b11c6ca19cf6b75b4ef54aeb5bd8
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-10 00:21:07 -0300

    feat(put-http): Agragamos put http method

--- PodoFrontGonnet/src/utils/http.js
+++ PodoFrontGonnet/src/utils/http.js
@@ -1,104 +1,127 @@
 export const get = async (url) => {
-    try {
-        const respuesta = await fetch(url)
+  try {
+    const respuesta = await fetch(url);
 
-        if (!respuesta.ok) {
-            throw new Error(respuesta.status + "error en fecth" + respuesta.statusText)
-        }
-        const data = await respuesta.json()
-
-        return data;
-
-    } catch (error) {
-        console.log(`error cath de get ${error}`)
+    if (!respuesta.ok) {
+      throw new Error(
+        respuesta.status + "error en fecth" + respuesta.statusText
+      );
     }
+    const data = await respuesta.json();
 
-}
-
+    return data;
+  } catch (error) {
+    console.log(`error cath de get ${error}`);
+  }
+};
 
 // get con token
 // get con token
 export const getToken = async (url, token) => {
-    try {
-        const fetchConfig = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        };
-        const respuesta = await fetch(url, fetchConfig);
+  try {
+    const fetchConfig = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const respuesta = await fetch(url, fetchConfig);
 
-        if (!respuesta.ok) {
-            throw new Error(respuesta.status + " error en fetch " + respuesta.statusText);
-        }
-        const data = await respuesta.json();
-        console.log(data)
-        return data;
-
-    } catch (error) {
-        console.log(`error catch de get ${error}`);
+    if (!respuesta.ok) {
+      throw new Error(
+        respuesta.status + " error en fetch " + respuesta.statusText
+      );
     }
+    const data = await respuesta.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(`error catch de get ${error}`);
+  }
 };
-
 
 export const post = async (url, data) => {
-    const fechConfig = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+  const fechConfig = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+  // creo que lo que falta, es poner en el post, el headers:header insertando el token digamos ,pero en la parte de la autorizacion del la peticion post
+
+  try {
+    const respuesta = await fetch(url, fechConfig);
+
+    if (!respuesta.ok) {
+      throw new Error(
+        respuesta.status + "error en fecth [post hhtp]" + respuesta.statusText
+      );
     }
-    // creo que lo que falta, es poner en el post, el headers:header insertando el token digamos ,pero en la parte de la autorizacion del la peticion post
+    const datos = await respuesta.json();
 
-    try {
-
-        const respuesta = await fetch(url, fechConfig)
-
-
-
-        if (!respuesta.ok) {
-            throw new Error(respuesta.status + "error en fecth [post hhtp]" + respuesta.statusText)
-        }
-        const datos = await respuesta.json()
-
-        return datos;
-
-    } catch (error) {
-        console.log(`error cath de get [POST HTTPS]${error}`)
-    }
-}
-
-export const postImagen = async (url, servicioPodo, token) => {
-    const formData = new FormData();
-    formData.append('nombre', servicioPodo.nombre); // Convertir objeto a JSON y agregar como parte
-    formData.append('descripcion', servicioPodo.descripcion); // Convertir objeto a JSON y agregar como parte
-    formData.append('costo', servicioPodo.costo); // Convertir objeto a JSON y agregar como parte
-    formData.append('file', servicioPodo.file); // Convertir objeto a JSON y agregar como parte
-
-    // Agregar archivo de imagen como parte
-
-
-    const fetchConfig = {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        body: formData
-    };
-
-    try {
-        const respuesta = await fetch(url, fetchConfig);
-
-        if (!respuesta.ok) {
-            throw new Error(respuesta.status + " error en fetch [post hhtp]: " + respuesta.statusText);
-        }
-
-        const datos = await respuesta.json();
-        console.log(datos);
-
-        return datos;
-    } catch (error) {
-        console.log("error cath de get [POST HTTPS]: " + error.statusText);
-    }
+    return datos;
+  } catch (error) {
+    console.log(`error cath de get [POST HTTPS]${error}`);
+  }
 };
 
+export const postImagen = async (url, servicioPodo, token) => {
+  const formData = new FormData();
+  formData.append("nombre", servicioPodo.nombre); // Convertir objeto a JSON y agregar como parte
+  formData.append("descripcion", servicioPodo.descripcion); // Convertir objeto a JSON y agregar como parte
+  formData.append("costo", servicioPodo.costo); // Convertir objeto a JSON y agregar como parte
+  formData.append("file", servicioPodo.file); // Convertir objeto a JSON y agregar como parte
+
+  // Agregar archivo de imagen como parte
+
+  const fetchConfig = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  };
+
+  try {
+    const respuesta = await fetch(url, fetchConfig);
+
+    if (!respuesta.ok) {
+      throw new Error(
+        respuesta.status +
+          " error en fetch [post hhtp]: " +
+          respuesta.statusText
+      );
+    }
+
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    return datos;
+  } catch (error) {
+    console.log("error cath de get [POST HTTPS]: " + error.statusText);
+  }
+};
+
+export const put = async (url, token) => {
+  const fechConfig = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    const respuesta = await fetch(url, fechConfig);
+
+    if (!respuesta.ok) {
+      throw new Error(
+        respuesta.status + "error en fecth [PUT hhtp]" + respuesta.statusText
+      );
+    }
+    const datos = await respuesta.json();
+
+    return datos;
+  } catch (error) {
+    console.log(`error cath de get [PUT HTTPS]${error}`);
+  }
+};
