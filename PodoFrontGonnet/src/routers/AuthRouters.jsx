cbaa98f36377abfcec0698e45ea08179b6baefc6
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-10 00:52:19 -0300

    feat(router): Correción en la ruta admin?

--- PodoFrontGonnet/src/routers/AuthRouters.jsx
+++ PodoFrontGonnet/src/routers/AuthRouters.jsx
@@ -3,6 +3,7 @@ import Inicio from "../pages/Inicio";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Servicios from "../pages/Servicios";
+import Dashboard from "./../pages/Dashboard";
 
 const AuthRouters = () => {
   return (
@@ -11,6 +12,7 @@ const AuthRouters = () => {
         <Route path="/" element={<Inicio />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
+        <Route path="/admin" element={<Dashboard />} />
         {/* <Route path="/servicio" element={<Servicios />} /> */}
         <Route path="/servicio/:id" element={<Servicios />} />
       </Routes>
