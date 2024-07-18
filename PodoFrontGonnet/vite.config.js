1ac19d532e822f1a0362bc0e8114f4bf5cbcf69f
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-18 12:21:07 -0300

    anda?

--- PodoFrontGonnet/vite.config.js
+++ PodoFrontGonnet/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Proyecto-PodoGonnet/PodoFrontGonnet/",
+  base: "/Proyecto-PodoGonnet/",
   plugins: [react()],
   css: {
     devSourcemap: true,
