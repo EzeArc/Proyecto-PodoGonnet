3af61e48084d0cc6e0df72f0a32d313140b2b4f2
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-18 14:09:10 -0300

    fix vite config

--- PodoFrontGonnet/vite.config.js
+++ PodoFrontGonnet/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL,
+  base:
+    process.env.NODE_ENV === "production"
+      ? process.env.VITE_BASE_URL || "/Proyecto-PodoGonnet"
+      : "/",
 
   plugins: [react()],
   css: {
