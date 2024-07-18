19f4f388ecbe5b17c8846e445c49ae1c78227fc4
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-17 23:14:31 -0300

    fix yml and gitignore

--- PodoFrontGonnet/vite.config.js
+++ PodoFrontGonnet/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Proyecto-PodoGonnet",
+  base: "https://github.com/EzeArc/Proyecto-PodoGonnet",
   plugins: [react()],
   css: {
     devSourcemap: true,
