1d72b7ff4cb0e19f2f870759f16656fff1a78100
Ezequiel <ezequielarce_1996@hotmail.com>
2024-07-18 18:01:42 -0300

    pruebasss

--- PodoFrontGonnet/vite.config.js
+++ PodoFrontGonnet/vite.config.js
@@ -2,10 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production"
-      ? process.env.VITE_BASE_URL || "/Proyecto-PodoGonnet"
-      : "/",
+  base: "/Proyecto-PodoGonnet/",
 
   plugins: [react()],
   css: {
